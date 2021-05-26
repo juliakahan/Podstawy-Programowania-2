bf74a80be486350f31608fb0c30175d70b4a99f7
Julia Kahan <57802539+juliakahan@users.noreply.github.com>
2021-05-26 23:31:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,41 +17,100 @@ int main()
 {
     //cout<<"hello"<<endl;
 
-    VectoredList v;
+//     VectoredList v;
 
-    for(int i=0;i<101;i++)
+//     for(int i=0;i<101;i++)
+//     {
+//         v.push_back(to_string(i));
+//     }
+//     for (int i = v.getSize()-1; i >= 0; i--)
+//     {
+//         cout<<v[i]<<" ";
+//     }
+//     cout<<"\n~~~~~~~~"<<endl;
+
+//     for(auto& element:v )
+//     {
+//         cout<<element<<' ';
+//     }
+//     cout<<"\n~~~~~~~~"<<endl;
+
+//     VectoredList::Iterator it = v.begin();
+//     it+=3;
+//     v.erase(it);
+//     v.show();
+//     cout<<"~~~~~~~~"<<endl;
+//     {
+//         VectoredList v2;
+//         for (int i = 0; i < 23; i++)
+//         {
+//             v2.push_back( to_string(24-i) );
+//         }
+//         v = v2;
+//     }
+//     v.show();
+//     cout<<"end"<<endl;
+
+
+ cout<<"Konstruktor przenoszacy"<<endl;
+    VectoredList v1;
+    for (int i = 0; i < 10; ++i)
     {
-        v.push_back(to_string(i));
+        v1.push_back(to_string(i));
     }
-    for (int i = v.getSize()-1; i >= 0; i--)
+
+    cout<<"v1"<<endl;
+    for (int i = 0; i < v1.getSize(); ++i)
     {
-        cout<<v[i]<<" ";
+        cout<<v1[i]<<" ";
     }
-    cout<<"\n~~~~~~~~"<<endl;
-
-    for(auto& element:v )
+    VectoredList v2 = move(v1);
+    cout<<endl;
+    cout<<"v2"<<endl;
+    for (int i = 0; i < v2.getSize(); ++i)
     {
-        cout<<element<<' ';
+        cout<<v2[i]<<" ";
     }
-    cout<<"\n~~~~~~~~"<<endl;
+    cout<<endl;
 
-    VectoredList::Iterator it = v.begin();
-    it+=3;
-    v.erase(it);
-    v.show();
-    cout<<"~~~~~~~~"<<endl;
+
+
+    cout<<"= przenoszacy"<<endl;
+
+    cout<<"v2"<<endl;
+    for (int i = 0; i < v2.getSize(); ++i)
     {
-        VectoredList v2;
-        for (int i = 0; i < 23; i++)
-        {
-            v2.push_back( to_string(24-i) );
-        }
-        v = v2;
+        cout<<v2[i]<<" ";
     }
-    v.show();
-    cout<<"end"<<endl;
+    cout<<endl;
+    VectoredList v3 =  move(v2);
+    cout<<"v3"<<endl;
+    for (int i = 0; i < v3.getSize(); ++i)
+    {
+        cout<<v3[i]<<" ";
+    }
+    cout<<endl;
 
 
+
+
+    cout<<"+ test (przenoszacy)"<<std::endl;
+    VectoredList v5;
+
+    for(int i=0;i<10;++i){
+        v5.push_back(to_string(i));
+    }
+    cout<<"v5"<<endl;
+    for(int i=0;i<v5.getSize();++i){
+        std::cout<<v5[i]<<" ";
+    }
+    cout<<endl;
+    VectoredList v6 =  move(v5);
+
+   cout<<"v4"<<endl;
+    for(int i=0; i< v6.getSize(); ++i){
+        cout<<v6[i]<<" ";
+    }
 
 
     return 0;
